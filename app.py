0b0d94ab68ec0dac46c7ed92e9fd6b025ffb48e1
ilyasweb <ilyasweb0721@gmail.com>
2021-08-25 11:46:06 -0700

    updated para

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 verb = input("Enter the verb? ")
 noun = input("Enter the noun? ")
 celebrity = input("Enter the celebrity name? ")
+paragraph = f"I was going for {verb} and I encountered {noun}, I decided to stay and watch {celebrity}"
 
-print(f"I was going for {verb} and I encountered {noun}, I decided to stay and watch {celebrity}")
+print(paragraph)

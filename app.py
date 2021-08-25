60f959e9c3f311adf9fd26b7599955d33b2e17a3
ilyasweb <ilyasweb0721@gmail.com>
2021-08-25 11:41:48 -0700

    updated code

---
+++ app.py
@@ -0,0 +1,5 @@
+verb = input("Enter the verb? ")
+noun = input("Enter the noun? ")
+celebrity = input("Enter the celebrity name? ")
+
+print(f"I was going for {verb} and I encountered {noun}, I decided to stay and watch {celebrity}")
